865046ef6a8b8c7c79f9dc9646d0f8686edefee6
MShnik <Mgpshnik@gmail.com>
2016-02-24 18:22:39 -0500

    Added kid model and header

---
+++ Classes/KidModel.cpp
@@ -0,0 +1,9 @@
+//
+//  KidModel.cpp
+//  Pineapple
+//
+//  Created by Michael Patashnik on 2/24/16.
+//
+//
+
+#include "KidModel.hpp"
---
+++ Classes/KidModel.h
@@ -0,0 +1,14 @@
+//
+//  KidModel.hpp
+//  Pineapple
+//
+//  Created by Michael Patashnik on 2/24/16.
+//
+//
+
+#ifndef KidModel_h
+#define KidModel_h
+
+#include <stdio.h>
+
+#endif /* KidModel_h */
