d2430ceb448e54befee3ad7d7f0cc1aac7fac519
ct457 <ct457@cornell.edu>
2016-05-05 11:54:41 +0800

    edited jello smushing

--- Classes/LoadingScreenController.cpp
+++ Classes/LoadingScreenController.cpp
@@ -24,7 +24,7 @@ void loadObstacles(TextureLoader* tloader) {
     tloader->loadAsync(PLATFORM_EDGE_RIGHT_TEXTURE,  "textures/platform-right.png");
     tloader->loadAsync(PLATFORM_EDGE_LEFT_TEXTURE,  "textures/platform-left.png");
     
-    tloader->loadAsync(JELLO_TEXTURE, "textures/jelloFull.png");
+    tloader->loadAsync(JELLO_TEXTURE, "textures/jelloFull2.png");
     tloader->loadAsync(SPIKE_TEXTURE, "textures/bowl_o_knives2.png");
     
     tloader->loadAsync(SPINNER_TEXTURE,   "textures/barrier.png");
